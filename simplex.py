5bb95e1b6730bcdc54f497bdcfe2b26e44766966
hamxa678 <hamxa678@gmail.com>
2025-03-20 04:45:58 +0100

    sdgsad

--- simplex.py
+++ simplex.py
@@ -79,7 +79,8 @@ class Simplex_CLASS:
         :param frequency: Frequency of initial octave of noise
         :return: Fractal noise sample with n lots of 2D images
         """
-        print(f't : {T}')
+        print(f't : {type(T)}')
+        print(f'T :: {type([T[0]])}')
         print(f'Notice :: shape :: {shape} T :: {T[0]}')
         assert len(shape) == 2
         # noise = np.zeros((1, *shape))
