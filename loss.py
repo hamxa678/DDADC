813a6214c08baaa26910bc72edaba950e23d0997
hamxa678 <hamxa678@gmail.com>
2025-03-20 12:14:16 +0100

    sdgsad

--- loss.py
+++ loss.py
@@ -102,7 +102,7 @@ def get_loss(model, x_0, t, config):
 
     # Apply forward diffusion process
     x_t = at.sqrt() * x_0 + (1 - at).sqrt() * e 
-    print(f"printing shape before model {x_t.shape}{t.shape}")
+    # print(f"printing shape before model {x_t.shape}{t.shape}")
     # Predict noise using the model
     output = model(x_t, t.float())
     loss = (e - output).square().mean(dim=(1, 2, 3)).mean(dim=0)
